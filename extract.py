457eae24f3b9a620d2978ba3e18b1a9602d3d0ac
Renan G. Dutra <r.granatodutra@gmail.com>
2023-09-29 01:04:40 -0300

    feat: add extract txt function

---
+++ extract.py
@@ -0,0 +1,9 @@
+def read_writing(path: str) -> str | None:
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+        content_lines = len(content.split("\n"))
+
+        if content_lines in range(7, 30):
+            return content
+        else:
+            print("A redação deve ter entre 7 e 30 linhas.")
